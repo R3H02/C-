56c5ea98bf72974fb83669a9152081dbcc53db2e
Renato Aguiar <renatoguiarsilva02@gmail.com>
2026-09-16 11:14:01 -0300

    Add commented code for circle area calculation

---
+++ area_circulo_1002.cs
@@ -0,0 +1,9 @@
+//area do circulo _ 1002
+
+//double raio, area;
+
+
+//raio = Convert.ToDouble(Console.ReadLine());
+//area = 3.14159 * (raio * raio);
+      
+//Console.Write("A={0}\n", area.ToString("0.0000"));
