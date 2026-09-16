a652cf81464945ac96a35f8f1c4ba0d09f05691e
Renato Aguiar <renatoguiarsilva02@gmail.com>
2026-09-16 11:12:40 -0300

    Add multiplicação_dois_valores_1004.cs file

---
+++ ---------multiplicação_dois_valores_1004.cs
@@ -0,0 +1,11 @@
+//---------multiplicação de dois valores_1004-------------
+
+//int A, B, resultado;
+
+
+//A = 3;
+//B = 9;
+
+//resultado = A * B;
+
+//Console.WriteLine("\nprod = " + (resultado));
