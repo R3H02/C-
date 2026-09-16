cbf3a304d5b4e0eadf08d662a693dc6a4a3e6198
Renato Aguiar <renatoguiarsilva02@gmail.com>
2026-09-16 11:21:13 -0300

    Create tomada_de_decisao.cs

---
+++ tomada_de_decisao.cs
@@ -0,0 +1,28 @@
+
+//tomada de decisão 
+
+int idade;
+idade = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Idade = {0} anos.\n", idade);
+// Decisao Simples, Composta, Encadeada, Multipla Escolha
+
+// DECISAO SIMPLES
+if (idade >= 12)
+{
+    // VERDADEIRO
+    Console.Write("Acesso liberado.\n");
+
+}
+// DECISAO COMPOSTA
+if (idade >= 12)
+{
+    // VERDADEIRO
+    Console.Write("Acesso liberado.\n");
+}
+else
+{
+    // FALSO
+    Console.Write("Acesso bloqueado\n");
+
+}
