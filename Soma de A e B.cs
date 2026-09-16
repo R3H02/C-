302a4073fe59fa03e8d0ae9da4be00b55c65aa02
Renato Aguiar <renatoguiarsilva02@gmail.com>
2026-09-16 09:53:30 -0300

    Add files via upload

---
+++ Soma de A e B.cs
@@ -0,0 +1,56 @@
+﻿//-------calculadora-------
+
+//int a, b;
+//double x;
+
+//Console.WriteLine("Calculadora.");
+
+//Console.Write("\nDigite o valor de a = ");
+//a = Convert.ToInt32(Console.ReadLine());
+
+//Console.Write("\nDigite o valor de b = ");
+//b = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("\nValor da soma = " + (a + b));
+//Console.WriteLine("Valor da subtração = " + (a - b));
+//Console.WriteLine("Valor da multiplicação = " + (a * b));
+
+//x = (double)a / b;
+
+//Console.WriteLine("Valor da divisão = " + x.ToString("0.00"));
+
+
+//---------soma simples-------
+
+//int a, b;
+//double x;
+
+
+//a = 10;
+
+//b = 2;
+
+//x = 10.5;
+//x = (int)x;
+
+
+//Console.WriteLine("\nx = " + (a + b));
+
+
+
+//----------soma de A e B------------
+
+
+
+int A, B, soma;
+Console.WriteLine("Soma de A e B");
+
+A = 10;
+B = 10;
+
+soma = A + B;
+
+Console.WriteLine("\n" + (soma));
+
+
+
